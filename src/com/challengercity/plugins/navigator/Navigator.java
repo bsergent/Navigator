ec80dc2c8e4fcb144e07bcae2fc3866b2062719d
Ben Sergent V <bsergent21897@gmail.com>
2015-04-10 23:00:20 -0400

    v0.0.2 Instructions
    +Base command with instructions on how to use
    +Version command

--- src/com/challengercity/plugins/navigator/Navigator.java
+++ src/com/challengercity/plugins/navigator/Navigator.java
@@ -4,7 +4,10 @@ package com.challengercity.plugins.navigator;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.logging.Level;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -16,7 +19,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Navigator extends JavaPlugin {
     
-    private final String version = "0.0.1";
+    private final String version = "0.0.2";
     
     @Override
     public void onEnable() {
@@ -50,6 +53,32 @@ public class Navigator extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().log(Level.INFO, "Navigator v{0} disabled.", version);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("navigator")) {
+            
+            if (args.length == 0 || (args.length < 2 && args[0].equals("1"))) {
+                sender.sendMessage(new String[] {
+                    ChatColor.GOLD+""+ChatColor.BOLD+"Navigator - Instructions",
+                    ChatColor.GOLD+"Waypoint:",
+                    ChatColor.WHITE+"  1x paper, 1x redstone"+(getConfig().getBoolean("require_enderpearl_for_crafting", true)?", 1x ender pearl":""),
+                    ChatColor.GOLD+"Setting a Waypoint:",
+                    ChatColor.WHITE+"  Right-click with a blank waypoint to create one at your current coords",
+                    ChatColor.GOLD+"Navigating to a Waypoint:",
+                    ChatColor.WHITE+"  Right-click with a set waypoint to navigate to it",
+                    ChatColor.GOLD+"Reset Destination to Spawn:",
+                    ChatColor.WHITE+"  Left-click with a compass",
+                });
+            }
+            
+            if (args.length >= 1 && args[0].equals("version")) {
+                sender.sendMessage(new String[] {ChatColor.GOLD+"Navigator v"+version, "Go to http://dev.bukkit.org/bukkit-plugins/navigator/ for updates."});
+            }
+        }
+        
+        return true;
     }
     
     public final class NavigationListener implements org.bukkit.event.Listener {
