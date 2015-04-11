5136387fc71526897f14decd390c24e48157ef08
Ben Sergent V <bsergent21897@gmail.com>
2015-04-11 01:27:50 -0400

    v0.0.3 Waypoint Mechanics
    +Waypoints are settable on right-click
    +Navigate to set waypoints
    +Compass can reset to spawn or show current destination
    +Waypoints save to item and navtargets.yml
    +Reset compass to last waypoint on login
    +Start of custom prefix support
    *Fixed waypoint material

--- src/com/challengercity/plugins/navigator/Navigator.java
+++ src/com/challengercity/plugins/navigator/Navigator.java
@@ -3,11 +3,16 @@ package com.challengercity.plugins.navigator;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.block.Action;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -19,7 +24,14 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Navigator extends JavaPlugin {
     
-    private final String version = "0.0.2";
+    private final String version = "0.0.3";
+    private String prefix = ChatColor.WHITE+"["+ChatColor.GOLD+"Nav"+ChatColor.WHITE+"]";
+    private org.bukkit.configuration.file.FileConfiguration navTargetsConfig = null;
+    private java.io.File navTargetsFile = null;
+    
+    // TODO Clone waypoints
+    // TODO Point to bed by default
+    // TODO Rename waypoint command option
     
     @Override
     public void onEnable() {
@@ -27,10 +39,10 @@ public class Navigator extends JavaPlugin {
         getConfig().options().copyDefaults(true);
         saveConfig();
         
-        ItemStack waypoint = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta wayPoint = (org.bukkit.inventory.meta.BookMeta) waypoint.getItemMeta();
-        wayPoint.setDisplayName("§rWaypoint");
-        wayPoint.setLore(Arrays.asList("§r§7Blank","§r§7Use the name of the recipient as the title"));
+        ItemStack waypoint = new ItemStack(Material.PAPER, 1);
+        ItemMeta wayPoint = waypoint.getItemMeta();
+        wayPoint.setDisplayName("Waypoint");
+        wayPoint.setLore(Arrays.asList("§r§7Blank","§r§7Right-click to set to current coords"));
         waypoint.setItemMeta(wayPoint);
         ShapelessRecipe waypointRecipe = new ShapelessRecipe(waypoint);
         waypointRecipe.addIngredient(Material.PAPER);
@@ -39,6 +51,15 @@ public class Navigator extends JavaPlugin {
             waypointRecipe.addIngredient(Material.ENDER_PEARL);
         }
         this.getServer().addRecipe(waypointRecipe);
+        
+        if (getConfig().getString("prefix") != null) {
+            prefix = getConfig().getString("prefix");
+        }
+        
+        if (navTargetsFile == null) {
+            navTargetsFile = new java.io.File(getDataFolder(), "navtargets.yml");
+        }
+        navTargetsConfig = org.bukkit.configuration.file.YamlConfiguration.loadConfiguration(navTargetsFile);
         
         try {
             org.mcstats.MetricsLite metrics = new org.mcstats.MetricsLite(this);
@@ -62,14 +83,15 @@ public class Navigator extends JavaPlugin {
             if (args.length == 0 || (args.length < 2 && args[0].equals("1"))) {
                 sender.sendMessage(new String[] {
                     ChatColor.GOLD+""+ChatColor.BOLD+"Navigator - Instructions",
-                    ChatColor.GOLD+"Waypoint:",
+                    ChatColor.GOLD+"Waypoint Crafting Recipe:",
                     ChatColor.WHITE+"  1x paper, 1x redstone"+(getConfig().getBoolean("require_enderpearl_for_crafting", true)?", 1x ender pearl":""),
                     ChatColor.GOLD+"Setting a Waypoint:",
                     ChatColor.WHITE+"  Right-click with a blank waypoint to create one at your current coords",
                     ChatColor.GOLD+"Navigating to a Waypoint:",
                     ChatColor.WHITE+"  Right-click with a set waypoint to navigate to it",
-                    ChatColor.GOLD+"Reset Destination to Spawn:",
-                    ChatColor.WHITE+"  Left-click with a compass",
+                    ChatColor.GOLD+"Compass Controls:",
+                    ChatColor.WHITE+"  Right-click to reset destination to spawm",
+                    ChatColor.WHITE+"  Left-click to show current destination",
                 });
             }
             
@@ -84,13 +106,82 @@ public class Navigator extends JavaPlugin {
     public final class NavigationListener implements org.bukkit.event.Listener {
         @org.bukkit.event.EventHandler(priority = org.bukkit.event.EventPriority.NORMAL)
         public void onPlayerInteractEvent(org.bukkit.event.player.PlayerInteractEvent e) {
-            // If waypoint
-            //   If blank, set to current pos
-            //   If has coords, set compass to coords
             
-            // If compass
-            //   If right-click, tell current destination
-            //   If left-click, clear and reset destination to spawn
+            if (e.getItem() != null) {
+                if (e.getItem().getType() == Material.COMPASS) {
+                    if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                        e.getPlayer().setCompassTarget(e.getPlayer().getWorld().getSpawnLocation());
+                        
+                        try { 
+                            navTargetsConfig.set(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".name", "Spawn");
+                            navTargetsConfig.set(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".x", e.getPlayer().getWorld().getSpawnLocation().getBlockX());
+                            navTargetsConfig.set(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".z", e.getPlayer().getWorld().getSpawnLocation().getBlockZ());
+
+                            navTargetsConfig.save(navTargetsFile);
+                        } catch (IOException ex) {
+                            getLogger().log(Level.INFO, "Failed to save {0}''s new navigation destination.", e.getPlayer().getName());
+                        }
+                        
+                        e.getPlayer().sendMessage(prefix+ChatColor.WHITE+" Compass destination reset to "+ChatColor.ITALIC+"Spawn"+ChatColor.RESET+ChatColor.WHITE+".");
+                        
+                    } else if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
+                        e.getPlayer().sendMessage(prefix+ChatColor.WHITE+" Current destination is "+ChatColor.ITALIC+navTargetsConfig.getString(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".name", "Spawn")+ChatColor.RESET+ChatColor.WHITE+".");
+                    }
+                } else if (e.getItem().getType() == Material.PAPER && (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+                    ItemStack paperStack = e.getItem();
+                    if (paperStack.hasItemMeta() && paperStack.getItemMeta().hasLore() && paperStack.getItemMeta().getLore().size() >= 1) {
+                        ItemMeta im = paperStack.getItemMeta();
+                        List<String> lore = im.getLore();
+                        if (lore.get(0).matches("^(.*)+x:[-0-9]+, z:[-0-9]+(.*)+$")) {
+                            String name = "Waypoint";
+                            if (im.hasDisplayName()) {
+                                name = im.getDisplayName().replaceFirst("§r|§o", "");
+                            }
+                                
+                            try {
+                                int x = Integer.parseInt(lore.get(0).replaceFirst("^(.*)(x|X):", "").replaceFirst(", (z|Z):[-0-9]+(.*)$", ""));
+                                int z = Integer.parseInt(lore.get(0).replaceFirst("^(.*)(x|X):[-0-9]+, (z|Z):", ""));
+                                
+                                navTargetsConfig.set(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".name", name);
+                                navTargetsConfig.set(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".x", x);
+                                navTargetsConfig.set(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".z", z);
+                                
+                                navTargetsConfig.save(navTargetsFile);
+                                
+                                e.getPlayer().setCompassTarget(new Location(e.getPlayer().getWorld(), x, 64, z));
+                            
+                                e.getPlayer().sendMessage(prefix+ChatColor.WHITE+" Destination set to "+ChatColor.ITALIC+name+ChatColor.RESET+ChatColor.WHITE+".");
+                                
+                            } catch (NullPointerException ex) {
+                               getLogger().log(Level.INFO, "Failed to parse {0}''s new navigation destination.", e.getPlayer().getName());
+                            } catch (IOException ex) {
+                                getLogger().log(Level.INFO, "Failed to save {0}''s new navigation destination.", e.getPlayer().getName());
+                            }
+                        } else if (lore.get(0).contains("Blank") || lore.get(0).contains("blank")) {
+                            int x = e.getPlayer().getLocation().getBlockX();
+                            int z = e.getPlayer().getLocation().getBlockZ();
+                            
+                            lore.set(0, "§r§7x:"+x+", z:"+z);
+                            if (lore.size() >= 2) {
+                                lore.set(1, "§r§7Right-click to set as destination");
+                            }
+                            im.setLore(lore);
+                            paperStack.setItemMeta(im);
+                            
+                            e.getPlayer().sendMessage(prefix+ChatColor.WHITE+" New waypoint set to "+x+", "+z);
+                            e.getPlayer().sendMessage(prefix+ChatColor.WHITE+" You can rename this waypoint in an anvil.");
+                        }
+                    }
+                }
+            }
+        }
+        
+        @org.bukkit.event.EventHandler(priority = org.bukkit.event.EventPriority.MONITOR)
+        public void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent e) {
+            if (navTargetsConfig.contains(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".name")) {
+                Location spawn = e.getPlayer().getWorld().getSpawnLocation();
+                e.getPlayer().setCompassTarget(new Location(e.getPlayer().getWorld(), navTargetsConfig.getInt(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".x", spawn.getBlockX()), 64, navTargetsConfig.getInt(e.getPlayer().getUniqueId()+"."+e.getPlayer().getWorld().getUID()+".z", spawn.getBlockZ())));
+            }
         }
     }
 }
